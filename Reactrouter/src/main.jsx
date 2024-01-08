a5c7ef6a52493ea97a5130d3374645dba8de8175
Sparsh Rawat <sparshrawat34@gmail.com>
2024-01-08 15:46:11 +0530

    About added

--- Reactrouter/src/main.jsx
+++ Reactrouter/src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider ,createBrowserRouter } from 'react-router-dom'
+import Layout from './assets/Layout.jsx'
+import Home from './assets/Components/Home/Home.jsx'
+import About from './assets/Components/About/About.jsx'
+
+const router=router.createBrowserRouter([
+    {
+      path:"/",
+      element:<Layout/>,
+      children:[
+        {
+          path:"",
+          element:<Home/>
+        },
+        {
+          path:"about",
+          element:<About/>
+        }
+      ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
