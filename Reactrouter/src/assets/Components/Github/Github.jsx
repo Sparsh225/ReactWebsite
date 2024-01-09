d1ed9c995ff40304763c23e31c232f8e3030e3e0
Sparsh Rawat <sparshrawat34@gmail.com>
2024-01-09 14:58:53 +0530

    github follwers fetch

--- Reactrouter/src/assets/Components/Github/Github.jsx
+++ Reactrouter/src/assets/Components/Github/Github.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Github() {
+    const [data,setData]=useState([])
+    useEffect(()=>{
+       fetch("https://api.github.com/users/Sparsh225")
+       .then(reslove=>reslove.json())
+       .then(data=>{
+        console.log(data)
+        setData(data)
+       })
+     },[])
   return (
     <div className='text-center m-4 text-white p-4 bg-gray-400 text-3xl'>
-      Github followers 
+      Github followers :{ data.followers}
+      <img width={300} src={data.avatar_url} alt="github_picture"  />
     </div>
   )
 }
